8bf180e7ff570c76b10cebf4c3c28b242c8b5610
aleesteele <aleesteele@gmail.com>
2018-04-22 17:07:37 -0400

    added line breaks to readme

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-Test project for PSCORE - Digital version of (North Korea 101)[]
+Test project for PSCORE - Digital version of [North Korea 101](https://doc-0s-4k-docs.googleusercontent.com/docs/securesc/17h97mh2ml43j3f07seqgnov6ojd737u/cs20cn4t95shvp8js0nflct7pni6nqjl/1524420000000/00743405216146583975/17009535664459547249/0B5CoBKWtNPLzajQ2X2NBWndBV28?e=view)
 
 Running on local machine from the command line:
 
-1. Clone branch.
+1. Clone branch.<br/>
+
 `git clone git@github.com:aleesteele/pscore-nk101.git`
-2. Install middleware
-`npm install`
-3. Run on local host.
-`npm start`
+
+2. Install middleware<br/>
+
+`npm install`<br/>
+
+3. Run on localhost.<br/>
+
+`npm start`<br/>
--- src/App.js
+++ src/App.js
@@ -13,16 +13,31 @@ class App extends Component {
             <div className="App-intro">
                 Korea split into North and South at the end of World War II, following Japan's defeat in 1945. This is a guide to North Korea, the most closed country in the world.
                 <br/>
-                <a href='#section1'> Go to section 1 </a>
-                <br/>
-                <a href='#section2'> Go to section 2 </a>
+                Click on each section to be taken to the description.
+                <ol text-al>
+                    <li><a href='#section1'> Demography</a></li>
+                    <li><a href='#section2'> Education</a></li>
+                    <li><a href='#section3'> Food</a></li>
+                </ol>
             </div>
             <div className="">
                 <ScrollableAnchor id={'section1'}>
-                    <div className="section1"> Hello World </div>
+                    <div className="section1">
+                        <h1>Facts</h1>
+                        <b>Population</b>: 25 million (South Korea has 50 million)<br/>
+                        <b>GDP per capita</b>: $1,800 (South Korea is $33,200)<br/>
+                        <b>GDP Spent on Military</b>: 25% (2.7% in South Korea)
+                    </div>
                 </ScrollableAnchor>
                 <ScrollableAnchor id={'section2'}>
-                    <div className="section2"> How are you world? </div>
+                    <div className="section2">
+                        <h1>Education</h1>
+                    </div>
+                </ScrollableAnchor>
+                <ScrollableAnchor id={'section3'}>
+                    <div className="section3">
+                        <h1>Food</h1>
+                    </div>
                 </ScrollableAnchor>
 
             </div>
