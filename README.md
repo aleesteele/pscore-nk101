7225a411e132ce144c3fae4fd7309dd127177112
aleesteele <aleesteele@gmail.com>
2018-04-22 16:16:32 -0400

    moving to public directory + testing scroll feature

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-Test project for PSCORE
+Test project for PSCORE - Digital version of (North Korea 101)[]
 
+Running on local machine from the command line:
+
+1. Clone branch.
+`git clone git@github.com:aleesteele/pscore-nk101.git`
+2. Install middleware
 `npm install`
-
-
+3. Run on local host.
 `npm start`
--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ScrollableAnchor from 'react-scrollable-anchor'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (<div className="App">
+            <header className="App-header">
+                <img src="PSCORE_Logo.png" className="App-logo" alt="logo"/>
+                <h1 className="App-title">North Korea 101</h1>
+            </header>
+            <div className="App-intro">
+                Korea split into North and South at the end of World War II, following Japan's defeat in 1945. This is a guide to North Korea, the most closed country in the world.
+                <br/>
+                <a href='#section1'> Go to section 1 </a>
+                <br/>
+                <a href='#section2'> Go to section 2 </a>
+            </div>
+            <div className="">
+                <ScrollableAnchor id={'section1'}>
+                    <div className="section1"> Hello World </div>
+                </ScrollableAnchor>
+                <ScrollableAnchor id={'section2'}>
+                    <div className="section2"> How are you world? </div>
+                </ScrollableAnchor>
+
+            </div>
+        </div>);
+    }
 }
 
 export default App;
